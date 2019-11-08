29a4750ada38abae7386ce8e8e55a2b937632d7a
testumyser0009 <testumyser0009@gmail.com>
2019-11-08 23:57:06 +0800

    a

--- public/index.php
+++ public/index.php
@@ -8,9 +8,14 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 'on');
 // [ 应用入口文件 ]
-
+header('Content-Type: text/html;charset=utf-8');
+header('Access-Control-Allow-Origin:*'); // *代表允许任何网址请求
+header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE'); // 允许请求的类型
+header('Access-Control-Allow-Credentials: true'); // 设置是否允许发送 cookies
+header('Access-Control-Allow-Headers: Content-Type,Content-Length,Accept-Encoding,X-Requested-with, Origin');
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
